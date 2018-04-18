b9423d6a3f95151d9093b454c14a93774ea5dbd9
SUAREZ REDONDO Rafael <rafael.suarez@soprasteria.com>
2018-04-19 00:31:48 +0200

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import {ImageService} from './services/image.service';
 
 import { AppComponent } from './app.component';
@@ -8,7 +9,15 @@ import { GalleryComponent } from './gallery/gallery.component';
 import { ImageListComponent } from './gallery/image-list/image-list.component';
 import { ImageComponent } from './gallery/image-list/image.component';
 import { ImageDetailComponent } from './gallery/image-detail/image-detail.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
+const appRoutes: Routes = [
+  { path: '', redirectTo:'/gallery', pathMatch: 'full'},
+  { path: 'gallery', component: GalleryComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
@@ -17,9 +26,12 @@ import { ImageDetailComponent } from './gallery/image-detail/image-detail.compon
     GalleryComponent,
     ImageListComponent,
     ImageComponent,
-    ImageDetailComponent
+    ImageDetailComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [ImageService],
