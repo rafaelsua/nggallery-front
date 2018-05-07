cf5787fa857e2b107ed45d8b3cd864e341539138
SUAREZ REDONDO Rafael <rafael.suarez@soprasteria.com>
2018-05-07 23:39:31 +0200

    Add admin list images with edit and delete buttoms

--- src/app/admin/admin-images-list/admin-images-list.component.ts
+++ src/app/admin/admin-images-list/admin-images-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from '../../models/image';
+import {ImageService} from '../../services/image.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'ng-admin-images-list',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminImagesListComponent implements OnInit {
 
-  constructor() { }
+  images: Observable<Image[]>;
+
+  constructor(private imageService: ImageService) { }
 
   ngOnInit() {
+    this.images = this.imageService.getImages();
   }
 
 }
